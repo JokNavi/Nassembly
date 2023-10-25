c88d130b68d7d7c0c79f7838d5162e78887d3a37
JokNavi <JokNavi1@gmail.com>
2023-10-25 18:46:01 +0200

    Add bash build script for asm

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
-# Nassembly
-Learning nasm assembly
-
-## Run Nassembly
-
-For my system/setup only*
-
-```sh
 #!/bin/bash
 nasm -f elf64 "${1}" -o "target/$(basename ${1%.*}).o"
 ld -o "target/$(basename ${1%.*})" "target/$(basename ${1%.*}).o"
-```
