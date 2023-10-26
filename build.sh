ee463cfe800977af025008174cfef19b4a933307
JokNavi <JokNavi1@gmail.com>
2023-10-26 17:57:43 +0200

    change .gitignore

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-nasm -f elf64 "${1}" -s -o "target/build/$(basename ${1%.*}).o"
-ld -o "target/bin/$(basename ${1%.*})" "target/build/$(basename ${1%.*}).o"
-cp "target/bin/$(basename ${1%.*})" "$(basename ${1%.*})"
