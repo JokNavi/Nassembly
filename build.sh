f78b6b5cce3e0b278deba822442ed2ed383db275
JokNavi <JokNavi1@gmail.com>
2023-10-26 17:10:57 +0200

    Add functions.asm and change build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-nasm -f elf64 "${1}" -o "target/$(basename ${1%.*}).o"
-ld -o "target/$(basename ${1%.*})" "target/$(basename ${1%.*}).o"
+nasm -f elf64 "${1}" -s -o "target/build/$(basename ${1%.*}).o"
+ld -o "target/bin/$(basename ${1%.*})" "target/build/$(basename ${1%.*}).o"
+cp "target/bin/$(basename ${1%.*})" "$(basename ${1%.*})"
--- src/functions.asm
+++ src/functions.asm
@@ -6,14 +6,19 @@ section .text
     global _start
 
 _start:
-    ; write(1, hello, 13)
-    mov eax, 4         ; syscall number for sys_write
-    mov ebx, 1         ; file descriptor 1 (stdout)
-    mov ecx, hello     ; pointer to the string
-    mov edx, helloLen  ; string length
-    int 0x80           ; interrupt to invoke syscall
 
-    ; exit(0)
-    mov eax, 60        ; syscall number for sys_exit
-    xor ebx, ebx       ; status: 0
-    syscall           ; interrupt to invoke syscall
+    times 2 call print_hello
+    ; call print_hello 2 times
+    
+    mov rax, 60         ; syscall number for sys_exit
+    xor rdi, rdi        ; status: 0
+    syscall             ; invoke syscall
+
+
+print_hello:
+    mov rax, 1          ; syscall number for write
+    mov rdi, 1          ; file descriptor 1 (stdout)
+    mov rsi, hello      ; pointer to the string
+    mov rdx, helloLen   ; string length
+    syscall             ; invoke syscall
+    ret                 ; return from fn
